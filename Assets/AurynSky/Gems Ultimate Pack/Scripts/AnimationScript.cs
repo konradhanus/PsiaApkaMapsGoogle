7cc150266af91bab013e9095a2953929de9f885b
Konrad Hanus <konrad.hanus@gmail.com>
2024-03-23 10:35:45 +0100

    flying gems

--- Assets/AurynSky/Gems Ultimate Pack/Scripts/AnimationScript.cs
+++ Assets/AurynSky/Gems Ultimate Pack/Scripts/AnimationScript.cs
@@ -8,6 +8,8 @@ public class AnimationScript : MonoBehaviour {
     public bool isRotating = false;
     public bool isFloating = false;
     public bool isScaling = false;
+    public bool isMovingToTop = true;
+    private GameObject topEdge;
 
     public Vector3 rotationAngle;
     public float rotationSpeed;
@@ -27,34 +29,48 @@ public class AnimationScript : MonoBehaviour {
 
     public float leftBoundary = -5f;
     public float rightBoundary = 5f;
+    public float speed = 2.0f; // Prędkość poruszania się obiektu
 
+ 
     // Use this for initialization
     void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        // Określ pozycję krawędzi górnej ekranu w przestrzeni świata
 
-       
-        
-        if(isAnimated)
+        topEdge = GameObject.Find("TopEdge");
+    }
+
+    // Metoda do zniszczenia obiektu z efektem pękania bańki
+    private void DestroyBubble()
+    {
+        // Tutaj możesz dodać efekt pękania bańki
+        // Na przykład animację, efekt dźwiękowy itp.
+
+        // Zniszcz obiekt
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        if (isAnimated)
         {
-            if(isRotating)
+            if (isRotating)
             {
                 transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
             }
 
-            if(isFloating)
+            if (isFloating)
             {
                 floatTimer += Time.deltaTime;
 
                 // Ustal wektor ruchu na podstawie aktualnej pozycji obiektu
-                Vector3 moveDir = new Vector3(0.0f, 0.0f, floatSpeed);
-                if (transform.position.x < leftBoundary)
-                    moveDir.x = floatSpeed;
-                else if (transform.position.x > rightBoundary)
-                    moveDir.x = -floatSpeed;
+                Vector3 moveDir = new Vector3(floatSpeed, 0.0f, 0.0f); // Zmiana na osią X
+                if (transform.position.y < leftBoundary)
+                    moveDir.y = floatSpeed;
+                else if (transform.position.y > rightBoundary)
+                    moveDir.y = -floatSpeed;
 
                 // Wykonaj translację obiektu
                 transform.Translate(moveDir * Time.deltaTime);
@@ -74,7 +90,25 @@ public class AnimationScript : MonoBehaviour {
                 }
             }
 
-            if(isScaling)
+
+            if (isMovingToTop)
+            {
+                // Pobierz lokalną przestrzeń osi Y w oparciu o aktualną rotację obiektu
+                Vector3 localUp = transform.TransformDirection(Vector3.forward);
+
+                // Przesuń obiekt wzdłuż lokalnej przestrzeni osi Y
+                transform.position += localUp * speed * Time.deltaTime;
+
+                float topEdgePositionY = topEdge.transform.position.y;
+                // Sprawdź, czy obiekt przekroczył górną krawędź ekranu
+                if (transform.position.y >= topEdgePositionY)
+                {
+                    // Zniszcz obiekt
+                    Destroy(gameObject);
+                }
+            }
+
+            if (isScaling)
             {
                 scaleTimer += Time.deltaTime;
 
@@ -87,7 +121,7 @@ public class AnimationScript : MonoBehaviour {
                     transform.localScale = Vector3.Lerp(transform.localScale, startScale, scaleSpeed * Time.deltaTime);
                 }
 
-                if(scaleTimer >= scaleRate)
+                if (scaleTimer >= scaleRate)
                 {
                     if (scalingUp) { scalingUp = false; }
                     else if (!scalingUp) { scalingUp = true; }
