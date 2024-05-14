3b4ae956a8f4923d4a60dfd442425ca346b784b3
Konrad Hanus <konrad.hanus@gmail.com>
2024-05-14 09:31:11 +0200

    running dog with forset

--- Assets/MovementDogControler.cs
+++ Assets/MovementDogControler.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class ButtonController : MonoBehaviour
 {
     public Animator animator; // Referencja do animatora
     public bool isPressed = false; // Flaga określająca, czy przycisk jest wciśnięty
+    private bool AttackReady_b = false; // Flaga określająca, czy atak jest gotowy
 
     public Button button; // Referencja do komponentu Button
 
@@ -52,4 +54,37 @@ public class ButtonController : MonoBehaviour
         // Ustawienie wartości parametru w animatorze
         //animator.SetBool("Sit_b", isPressed);
     }
+
+    public void Jump()
+    {
+        //  Vector3 currentPosition = gameObject.transform.position;
+
+        // // Dodaj dodatkową wysokość do wartości Y
+        // float additionalHeight = .5f; // Możesz dostosować tę wartość według potrzeb
+        // currentPosition.y += additionalHeight;
+
+        // // Ustaw nową pozycję obiektu
+        // gameObject.transform.position = currentPosition;
+        // Wywołanie triggera "Jump" w animatorze
+        animator.SetTrigger("Jump_tr");
+    }
+
+
+    public void Attack()
+    {
+        // Ustawienie flagi na true na pół sekundy
+        //  animator.SetBool("AttackReady_b", true);
+        StartCoroutine(EnableAttackForHalfSecond());
+
+        // Tutaj można umieścić kod odpowiedzialny za atak
+    }
+
+    private IEnumerator EnableAttackForHalfSecond()
+    {
+
+        animator.SetBool("AttackReady_b", true);
+        yield return new WaitForSeconds(1f);
+        animator.SetBool("AttackReady_b", false);
+    }
+    
 }
