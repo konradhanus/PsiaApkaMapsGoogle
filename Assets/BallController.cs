fe4e5f62791fe9a86f9a5c85e9eedd8b57bde47d
Konrad Hanus <konrad.hanus@gmail.com>
2024-05-28 09:55:25 +0200

    success throw a ball screen

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public bool hasTouchedGround = false;
-     public float groundHeight = 0.1f;
+    public float groundHeight = 0.1f;
+    public SliderController sliderController; // Dodajemy referencję do SliderController
+
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             hasTouchedGround = true;
+
+            if (sliderController != null)
+            {
+                sliderController.UpdateProgress(); // Wywołujemy metodę UpdateProgress
+
+            }
+            else {
+                Debug.Log("progress nie ma");
+            }
+
         }
     }
 
