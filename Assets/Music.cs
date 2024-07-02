08dc2e660542133eaf45b50173307b83ce8b6260
Konrad Hanus <konrad.hanus@gmail.com>
2024-07-02 08:00:36 +0200

    first screen small mobile version

--- Assets/Music.cs
+++ Assets/Music.cs
@@ -1,55 +1,36 @@
-using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class MusicPlayer : MonoBehaviour
+public class MusicManager : MonoBehaviour
 {
-    public AudioClip musicClip;
-    public float fadeOutDuration = 1.0f;
-    
-    private static AudioSource audioSource;
-    private static bool isFadingOut = false;
+    public AudioClip songTheme;
+    public AudioClip soundtrack;
+    public GameObject loadingObject;
 
-    void Awake()
+    private AudioSource audioSource;
+
+    void Start()
     {
-        if (audioSource == null)
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (loadingObject.activeSelf)
         {
-            audioSource = GetComponent<AudioSource>();
-            audioSource.clip = musicClip;
-            audioSource.loop = true;
+            PlayClip(songTheme);
+        }
+        else
+        {
+            PlayClip(soundtrack);
+        }
+    }
+
+    private void PlayClip(AudioClip clip)
+    {
+        if (audioSource.clip != clip)
+        {
+            audioSource.clip = clip;
             audioSource.Play();
         }
-    }
-
-    void OnEnable()
-    {
-        if (!audioSource.isPlaying && !isFadingOut)
-        {
-            audioSource.Play();
-        }
-    }
-
-    void OnDisable()
-    {
-        if (audioSource.isPlaying && !isFadingOut)
-        {
-            StartCoroutine(FadeOut());
-        }
-    }
-
-    private IEnumerator FadeOut()
-    {
-        isFadingOut = true;
-        float startVolume = audioSource.volume;
-
-        while (audioSource.volume > 0)
-        {
-            audioSource.volume -= startVolume * Time.deltaTime / fadeOutDuration;
-            yield return null;
-        }
-
-        audioSource.Stop();
-        audioSource.volume = startVolume;
-        isFadingOut = false;
     }
 }
