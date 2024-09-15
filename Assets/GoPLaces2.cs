2a9678fff8dac1ea0ee21f5810aa5ecd50b7d037
Konrad Hanus <konrad.hanus@gmail.com>
2024-09-15 13:54:49 -0700

    working dogspots and dogyms on the map

--- Assets/GoPLaces2.cs
+++ Assets/GoPLaces2.cs
@@ -11,7 +11,7 @@ namespace GoMap
     {
         public GOMap goMap;
         public GameObject prefab;
-        private GameObject placedObject;
+        private List<GameObject> placedObjects = new List<GameObject>();
 
         // Warto?ci latitude, longitude i promienia
         float latitude = 51.08666657545862f;
@@ -19,30 +19,52 @@ namespace GoMap
         float radius = 0.5f;
 
         // Adres URL endpointu
-        public string endpointURL = "https://psiaapka.pl/psiaapka/dogspots.php";
+        public string endpointURL = "https://psiaapka.pl/psiaapka/dogspots_v2.php";
 
         List<DataObject> dataObjects;
 
+        // Parametr debugowania
+        public bool debug = true; // W??czony debug
+
         void Awake()
         {
-            goMap.OnTileLoad.AddListener((GOTile) => {
-                OnLoadTile(GOTile);
+            if (debug) Debug.Log("GO_PLACES2: Awake - Initializing.");
+            goMap.OnTileLoad.AddListener((GOTile tile) => {
+                if (debug) Debug.Log($"GO_PLACES2: OnTileLoad - Tile loaded: {tile}");
+                StartCoroutine(OnLoadTile(tile));
             });
         }
 
-        void OnLoadTile(GOTile tile)
+        IEnumerator OnLoadTile(GOTile tile)
         {
-            // Only place objects if they haven't been placed yet
-            if (placedObject == null)
+            if (debug)
             {
-                StartCoroutine(FetchData(tile));
+                Debug.Log($"GO_PLACES2: OnLoadTile - Starting for tile: {tile}");
+                Debug.Log($"GO_PLACES2: Map Tile Coordinates (Lat, Lon): {tile.goTile.tileCenter.latitude}, {tile.goTile.tileCenter.longitude}");
+            }
+            yield return StartCoroutine(FetchData(tile));
+
+            if (debug) Debug.Log("GO_PLACES2: OnLoadTile - Data fetched, placing objects.");
+
+            foreach (var dataObject in dataObjects)
+            {
+                PlaceObjectOnMap(tile, dataObject);
             }
         }
 
         IEnumerator FetchData(GOTile tile)
         {
-            // Tworzenie zapytania URL
-            string url = $"{endpointURL}?latitude={latitude}&longitude={longitude}&radius={radius}";
+            if (debug) Debug.Log("GO_PLACES2: FetchData - Starting data fetch.");
+
+            // Wy?wietlenie wsp??rz?dnych uderzaj?cych do endpointa
+            string latitudeStr = tile.goTile.tileCenter.latitude.ToString().Replace(',', '.');
+            string longitudeStr = tile.goTile.tileCenter.longitude.ToString().Replace(',', '.');
+            string radiusStr = radius.ToString().Replace(',', '.');
+
+            // Tworzenie URL z poprawnym formatowaniem
+            string url = $"{endpointURL}?latitude={latitudeStr}&longitude={longitudeStr}&radius={radiusStr}";
+            if (debug) Debug.Log($"GO_PLACES2: FetchData - Fetching from URL: {url}");
+
             UnityWebRequest request = UnityWebRequest.Get(url);
             yield return request.SendWebRequest();
 
@@ -52,46 +74,53 @@ namespace GoMap
                 yield break;
             }
 
-            // Pobranie odpowiedzi w formie JSON
             string jsonResponse = request.downloadHandler.text;
-            dataObjects = JsonConvert.DeserializeObject<List<DataObject>>(jsonResponse);
+            if (debug) Debug.Log($"GO_PLACES2: FetchData - Data fetched: {jsonResponse.Length} characters.");
 
-            foreach (var dataObject in dataObjects)
-            {
-                // Stw?rz obiekt na podstawie danych
-                PlaceObjectOnMap(tile, dataObject);
-            }
+            dataObjects = JsonConvert.DeserializeObject<List<DataObject>>(jsonResponse);
+            if (debug) Debug.Log($"GO_PLACES2: FetchData - Parsed {dataObjects.Count} objects.");
         }
 
         void PlaceObjectOnMap(GOTile tile, DataObject dataObject)
         {
-            // Konwersja wsp??rz?dnych z obiektu dataObject
+            if (debug) Debug.Log($"GO_PLACES2: PlaceObjectOnMap - Placing object: {dataObject.name}");
+
             Coordinates coordinates = new Coordinates(dataObject.latitude, dataObject.longitude, 0);
 
-            // Sprawd?, czy dane wsp??rz?dne nale?? do aktualnie wczytywanego kafla
             if (TileFilter(tile, coordinates))
             {
-                // Instantiate prefab
-                placedObject = Instantiate(prefab);
-                placedObject.SetActive(true);
+                GameObject newObject = Instantiate(prefab);
+                newObject.SetActive(true);
 
-                // Convert coordinates to position
-                Vector3 position = coordinates.convertCoordinateToVector(placedObject.transform.position.y);
+                Vector3 position = coordinates.convertCoordinateToVector(newObject.transform.position.y);
 
                 if (goMap.useElevation)
                     position = GOMap.AltitudeToPoint(position);
 
-                // Set the position of the object
-                placedObject.transform.localPosition = position;
-                placedObject.transform.SetParent(tile.transform);
-                placedObject.name = dataObject.name;
+                newObject.transform.localPosition = position;
+                newObject.transform.SetParent(tile.transform);
+                newObject.name = dataObject.name;
+
+                if (debug)
+                {
+                    Debug.Log($"GO_PLACES2: PlaceObjectOnMap - Object {dataObject.name} placed at {position}.");
+                    Debug.Log($"GO_PLACES2: PlaceObjectOnMap - DataObject Coordinates (Lat, Lon): {dataObject.latitude}, {dataObject.longitude}");
+                }
+
+                placedObjects.Add(newObject);
+            }
+            else
+            {
+                if (debug) Debug.Log($"GO_PLACES2: PlaceObjectOnMap - Object {dataObject.name} not placed, outside tile.");
             }
         }
 
         bool TileFilter(GOTile tile, Coordinates coordinates)
         {
             Vector2 tileCoordinates = coordinates.tileCoordinates(goMap.zoomLevel);
-            return tile.goTile.tileCoordinates.Equals(tileCoordinates);
+            bool isInTile = tile.goTile.tileCoordinates.Equals(tileCoordinates);
+            if (debug) Debug.Log($"GO_PLACES2: TileFilter - Coordinates {coordinates} in tile: {isInTile}");
+            return isInTile;
         }
 
         [System.Serializable]
