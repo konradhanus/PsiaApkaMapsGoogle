5b9835969599959890b3e99da0526f020331159b
Konrad Hanus <konrad.hanus@gmail.com>
2024-09-15 12:25:23 -0700

    dogspots

--- Assets/GoPLaces2.cs
+++ Assets/GoPLaces2.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
 using GoShared;
 
 namespace GoMap
@@ -8,11 +11,20 @@ namespace GoMap
     {
         public GOMap goMap;
         public GameObject prefab;
-        private GameObject placedObject; // Store the reference to the instantiated object
+        private GameObject placedObject;
+
+        // Warto?ci latitude, longitude i promienia
+        float latitude = 51.08666657545862f;
+        float longitude = 17.05298076897365f;
+        float radius = 0.5f;
+
+        // Adres URL endpointu
+        public string endpointURL = "https://psiaapka.pl/psiaapka/dogspots.php";
+
+        List<DataObject> dataObjects;
 
         void Awake()
         {
-            // Register to the GOMap event OnTileLoad
             goMap.OnTileLoad.AddListener((GOTile) => {
                 OnLoadTile(GOTile);
             });
@@ -20,54 +32,76 @@ namespace GoMap
 
         void OnLoadTile(GOTile tile)
         {
-            // Only place the object if it hasn't been placed yet
+            // Only place objects if they haven't been placed yet
             if (placedObject == null)
             {
-                NearbySearch(tile);
+                StartCoroutine(FetchData(tile));
             }
         }
 
-        void NearbySearch(GOTile tile)
+        IEnumerator FetchData(GOTile tile)
         {
-            Debug.Log("TILE");
+            // Tworzenie zapytania URL
+            string url = $"{endpointURL}?latitude={latitude}&longitude={longitude}&radius={radius}";
+            UnityWebRequest request = UnityWebRequest.Get(url);
+            yield return request.SendWebRequest();
 
-            // Set coordinates (latitude and longitude)
-            double lat = 51.086570739746094;
-            double lng = 17.053138732910156;
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(request.error);
+                yield break;
+            }
 
-            // Create a new coordinate object with the desired lat/lon
-            Coordinates coordinates = new Coordinates(lat, lng, 0);
+            // Pobranie odpowiedzi w formie JSON
+            string jsonResponse = request.downloadHandler.text;
+            dataObjects = JsonConvert.DeserializeObject<List<DataObject>>(jsonResponse);
 
+            foreach (var dataObject in dataObjects)
+            {
+                // Stw?rz obiekt na podstawie danych
+                PlaceObjectOnMap(tile, dataObject);
+            }
+        }
+
+        void PlaceObjectOnMap(GOTile tile, DataObject dataObject)
+        {
+            // Konwersja wsp??rz?dnych z obiektu dataObject
+            Coordinates coordinates = new Coordinates(dataObject.latitude, dataObject.longitude, 0);
+
+            // Sprawd?, czy dane wsp??rz?dne nale?? do aktualnie wczytywanego kafla
             if (TileFilter(tile, coordinates))
             {
-                // Instantiate your game object if it hasn't been instantiated yet
+                // Instantiate prefab
                 placedObject = Instantiate(prefab);
                 placedObject.SetActive(true);
 
                 // Convert coordinates to position
                 Vector3 position = coordinates.convertCoordinateToVector(placedObject.transform.position.y);
-                Debug.Log("TILE Converted Position: " + position.ToString());
 
                 if (goMap.useElevation)
                     position = GOMap.AltitudeToPoint(position);
 
                 // Set the position of the object
                 placedObject.transform.localPosition = position;
-
-                // Set the parent of the object
                 placedObject.transform.SetParent(tile.transform);
-
-                // Optionally set the name of the object
-                placedObject.name = "PlacedObject";
+                placedObject.name = dataObject.name;
             }
         }
 
         bool TileFilter(GOTile tile, Coordinates coordinates)
         {
             Vector2 tileCoordinates = coordinates.tileCoordinates(goMap.zoomLevel);
-
-            // Ensure the coordinates are within the tile
             return tile.goTile.tileCoordinates.Equals(tileCoordinates);
+        }
+
+        [System.Serializable]
+        public class DataObject
+        {
+            public string id;
+            public float latitude;
+            public float longitude;
+            public string type;
+            public string name;
         }
     }
 }
