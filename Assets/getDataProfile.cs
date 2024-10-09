e5d9670132ce710be9fed2f392c2fbe46bf25776
Konrad Hanus <konrad.hanus@gmail.com>
2024-10-09 12:41:41 -0700

    small fixes with steps

--- Assets/getDataProfile.cs
+++ Assets/getDataProfile.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class DateHandler : MonoBehaviour
 {
-
     public TextMeshProUGUI dateTextMeshPro;
 
     // Referencja do skryptu HealthStepAndDistance
     public HealthStepAndDistance healthStepAndDistance;
+
+    private float timer = 0f;  // Licznik czasu
 
     void Start()
     {
@@ -18,9 +19,41 @@ public class DateHandler : MonoBehaviour
             // Pobierz datę jako string z TextMeshPro
             string dateString = dateTextMeshPro.text;
 
-          
-                // Wywołaj metodę TotalStepCounter z klasy HealthStepAndDistance
-                healthStepAndDistance.TotalStepCounter(dateString);  
+            // Wywołaj metodę TotalStepCounter z klasy HealthStepAndDistance
+            healthStepAndDistance.TotalStepCounter(dateString);
+
+            // Rozpocznij wywoływanie GetAllStepsSinceJoin co 0.5 sekundy
+            InvokeRepeating("GetAllStepsSinceJoin", 0.5f, 0.5f);
+        }
+        else
+        {
+            Debug.LogError("Brak referencji do GameObject, TextMeshPro lub HealthStepAndDistance.");
+        }
+    }
+
+    void Update()
+    {
+        // Zwiększ licznik czasu o czas, który upłynął od ostatniej ramki
+        timer += Time.deltaTime;
+
+        // Sprawdź, czy minęło 3 sekund
+        if (timer >= 3f)
+        {
+            // Zatrzymaj wywoływanie GetAllStepsSinceJoin
+            CancelInvoke("GetAllStepsSinceJoin");
+        }
+    }
+
+    public void GetAllStepsSinceJoin()
+    {
+        // Sprawdź, czy podano wszystkie referencje
+        if (dateTextMeshPro != null && healthStepAndDistance != null)
+        {
+            // Pobierz datę jako string z TextMeshPro
+            string dateString = dateTextMeshPro.text;
+
+            // Wywołaj metodę TotalStepCounter z klasy HealthStepAndDistance
+            healthStepAndDistance.TotalStepCounter(dateString);
         }
         else
         {
